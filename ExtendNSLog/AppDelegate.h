11f4d49736243431e6048f6fe9b3da8966d687d3
Zhouzq <Zhouzq@cncn.net>
2013-09-12 11:18:31 +0800

    Initial Commit

---
+++ ExtendNSLog/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ExtendNSLog
+//
+//  Created by cncn on 13-9-12.
+//  Copyright (c) 2013年 cncn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ExtendNSLog/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ExtendNSLog
+//
+//  Created by cncn on 13-9-12.
+//  Copyright (c) 2013年 cncn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
