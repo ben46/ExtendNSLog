e11aaad5b80db8a831b22d0e1cc81fd7adcac45b
zhou <ben02060846@gmail.com>
2014-03-08 13:29:34 +0800

    Add NSLogRect, NSLogSize, NSLogPoint

--- ExtendNSLog/Classes/ExtendNSLogFunctionality.h
+++ ExtendNSLog/Classes/ExtendNSLogFunctionality.h
@@ -9,6 +9,9 @@
 #import <Foundation/Foundation.h>
 
 #ifdef DEBUG
+#define NSLogRect(rect) NSLog(@"%s x:%.4f, y:%.4f, w:%.4f, h:%.4f", #rect, rect.origin.x, rect.origin.y, rect.size.width, rect.size.height)
+#define NSLogSize(size) NSLog(@"%s w:%.4f, h:%.4f", #size, size.width, size.height)
+#define NSLogPoint(point) NSLog(@"%s x:%.4f, y:%.4f", #point, point.x, point.y)
 #define NSLog(args...) ExtendNSLog(__FILE__,__LINE__,__PRETTY_FUNCTION__,args);
 #else
 #define NSLog(x...)
